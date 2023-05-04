7f023c5a7e53c30fc243f8800cd1c97701e7fb55
Taha Shakir <tahashakir40@gmail.com>
2023-05-04 22:49:51 +0500

    Updated main and providers.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_instance" "dummy-server" {
+    ami = "ami-02396cdd13e9a1257"
+    instance_type = "t2.micro"
+    tags = {
+        name = "Your-DevOps-Mentor-tutorial"
+    }
 }
 
