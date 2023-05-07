b8d3901ae673edf595a0bf018e4dd6ea248f9d21
Taha Shakir <tahashakir40@gmail.com>
2023-05-07 15:58:55 +0500

    Adding private instance

--- instance.tf
+++ instance.tf
@@ -31,7 +31,43 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "${var.env_code}-SG"
+    Name = "${var.env_code}-SG-pub"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                        = "ami-0889a44b331db0194"
+  instance_type              = "t3.micro"
+  vpc_security_group_ids     = [aws_security_group.private.id]
+  subnet_id                  = aws_subnet.private[0].id
+  key_name                   = "terraform-practice"
+  tags = {
+    Name = "Private-${var.env_code}"
+  }
+
+}
+
+resource "aws_security_group" "private" {
+  name        = "Private-${var.env_code}"
+  description = "Allow VPC traffic"
+  vpc_id         = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    description = "SSH"
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    description = "Allow All outbound traffic"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env_code}-SG-pvt"
   }
 }
 
