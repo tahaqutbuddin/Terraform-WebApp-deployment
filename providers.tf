ab25dcccf98e4e7c271f1214897d5eff7a24e60d
Taha Shakir <tahashakir40@gmail.com>
2023-05-20 23:23:41 +0500

    Remote terraform state file

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "responsive-filemanager"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
